a0e2ae16974636c0877238f14aa92c8e8f07bb44
yeyintminoo <yeyint25.mk@gmail.com>
2020-08-18 16:14:13 +0630

    uncomment seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
-        // $this->call(BrandSeeder::class);
-        // $this->call(CategorySeeder::class);
-        // $this->call(SubcategorySeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(BrandSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(SubcategorySeeder::class);
          
         $this->call(UserSeeder::class);
     }
